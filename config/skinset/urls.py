369ee108f29cf3ae17bd92bc2267542f68d307fd
tokageel <eel.tkg@gmail.com>
2018-11-29 14:38:50 +0900

    Update views for the skinset elements.

--- config/skinset/urls.py
+++ config/skinset/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'skinset'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('category/<category_name>', views.list, name='list'),
-    path('dbimg/<int:image_id>', views.dbimg, name='dbimg'),
+    path('list/image/<category_name>', views.list_image, name='list_image'),
+    path('list/skin/<category_name>', views.list_skin, name='list_skin'),
+    path('img/image/<int:image_id>', views.img_image, name='img_image'),
+    path('img/skin/<int:skin_id>', views.img_skin, name='img_skin'),
     re_path(r'^.*.tga$', views.img, name='img'),
 ]
--- config/skinset/views.py
+++ config/skinset/views.py
@@ -4,7 +4,7 @@ from PIL import Image
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
-from .models import CropImage
+from .models import CropImage, SkinImage
 
 
 def index(request):
@@ -12,15 +12,29 @@ def index(request):
         .values('category') \
         .order_by('category') \
         .distinct()
-    context = {'category_list': category_list}
+    skin_list = SkinImage.objects\
+        .values('skin_category') \
+        .order_by('skin_category') \
+        .distinct()
+    context = {
+        'category_list': category_list,
+        'skin_list': skin_list
+    }
     return render(request, 'skinset/index.html', context)
 
 
-def list(request, category_name):
+def list_image(request, category_name):
     image_list = CropImage.objects.filter(category=category_name).order_by('file').all()
     context = {'category_name': category_name,
                'image_list': image_list}
-    return render(request, 'skinset/list.html', context)
+    return render(request, 'skinset/list_image.html', context)
+
+
+def list_skin(request, category_name):
+    image_list = SkinImage.objects.filter(skin_category=category_name).order_by('skin_name').all()
+    context = {'category_name': category_name,
+               'image_list': image_list}
+    return render(request, 'skinset/list_skin.html', context)
 
 
 def img(request):
@@ -30,10 +44,19 @@ def img(request):
     return res
 
 
-def dbimg(request, image_id):
+def img_image(request, image_id):
     record = CropImage.objects.filter(id=image_id).all()[0]
-    im = Image.open('./static/skinset/ui.ipf/' + record.file)
-    l, t, w, h = [int(s) for s in record.imgrect.split()]
+    return crop_img(request, record.file, record.imgrect)
+
+
+def img_skin(request, skin_id):
+    record = SkinImage.objects.filter(id=skin_id).all()[0]
+    return crop_img(request, record.texture, record.imgrect)
+
+
+def crop_img(request, file, rect):
+    l, t, w, h = [int(s) for s in rect.split()]
+    im = Image.open('./static/skinset/ui.ipf/' + file)
     im = im.crop((l, t, (l + w), (t + h)))
     res = HttpResponse(content_type='image/png')
     im.save(res, 'PNG')
